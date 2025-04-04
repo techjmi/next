97b633643d905a9cddb31fae68c9b14092040261
techjmi <contactshamim62@gmail.com>
2025-04-04 19:25:21 +0530

    xyz

--- src/app/addproduct/page.js
+++ src/app/addproduct/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function () {
+export default function AddproductPage() {
   const [formData, setFormData] = useState({
     name: "",
     price: "",
